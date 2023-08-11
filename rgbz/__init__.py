a39ee35679a691708515a9b51a23ada1daaa6c7e
Kevin Mcguigan <36888812+kkmcgg@users.noreply.github.com>
2023-08-11 11:54:05 -0300

    Update __init__.py

--- rgbz/__init__.py
+++ rgbz/__init__.py
@@ -1,1 +1,33 @@
-print('rbgz initialized. This is a work in progress. See https://github.com/kkmcgg/rgbz/ for more details')
+"""
+RGBZ - A Python package for manipulating and working with RGBZ color data.
+
+Author: Your Name
+License: Your License (e.g., MIT)
+"""
+
+# Import main functionalities for easier access to end users
+from .core import RGBZColor, some_rgbz_function
+from .io import read_rgbz_file, write_rgbz_file
+from .visualizer import visualize_rgbz
+
+# Define package-wide constants (if they exist in constants.py)
+from .constants import MAX_VALUE, MIN_VALUE
+
+# Optionally, if you have custom exceptions you want users to access easily
+from .exceptions import RGBZError, RGBZValueError
+
+# Define what gets imported with "from rgbz import *"
+__all__ = [
+    "RGBZColor", 
+    "some_rgbz_function",
+    "read_rgbz_file",
+    "write_rgbz_file",
+    "visualize_rgbz",
+    "MAX_VALUE",
+    "MIN_VALUE",
+    "RGBZError",
+    "RGBZValueError"
+]
+
+# Define package's version number
+__version__ = "0.1.0"
